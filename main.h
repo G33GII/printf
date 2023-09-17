9dfebb3ead80aba926b748ab0140851f830845c6
G33GII <gazasolomon@gmail.com>
2023-09-17 13:12:34 +0100

    Capitalizing the check in the header file

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_H
+#define MAIN_H
 /**
  * main - entry point
  * description - headers
@@ -7,4 +7,7 @@
 */
 int _printf(const char *format, ...);
 int _putchar(char a);
+
+
+
 #endif
