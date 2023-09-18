cec700f4914b1879cfe61442d4c1674cfd17eccb
G33GII <gazasolomon@gmail.com>
2023-09-18 07:33:04 +0100

    modified:   _printfII.c

--- _printfII.c
+++ _printfII.c
@@ -12,7 +12,7 @@ int _printf(const char *const format, ...)
 	const char *fmt_ptr = format;   /* Pointer to the format string */
 
 	if (!format)
-		return (1);
+		return (-1);
 
 	va_start(_list_, format);       /* Start processing the variable arguments */
 
