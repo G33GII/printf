da5ab02f4875bcc26454f3b3aa9c90b0d2845ab0
G33GII <gazasolomon@gmail.com>
2023-09-18 09:36:49 +0100

    modified:   _printfII.c

--- _printfII.c
+++ _printfII.c
@@ -6,7 +6,7 @@
  */
 int _printf(const char *const format, ...)
 {
-	int _buff_count = 0;             /* Character count in the buffer */
+	int _buff_ct = 0;             /* Character count in the buffer */
 	char _bff_[_bff_sz];            /* Buffer to store formatted string */
 	va_list _list_;                 /* Initialize a variable argument list */
 	const char *fmt_ptr = format;   /* Pointer to the format string */
@@ -22,25 +22,33 @@ int _printf(const char *const format, ...)
 		{
 			fmt_ptr++; /* Move to the character after '%' */
 			if (*fmt_ptr == '%')
-				_bff_[_buff_count++] = '%';
+				_bff_[_buff_ct++] = '%';
 			else if (*fmt_ptr == 'c') /* Character format specifier */
 			{
-				_bff_[_buff_count++] = va_arg(_list_, int);
+				_bff_[_buff_ct++] = va_arg(_list_, int);
 			}
 			else if (*fmt_ptr == 's') /* String format specifier */
 			{
 				char *str = va_arg(_list_, char *);
 
 				for (; *str; str++)
-					_bff_[_buff_count++] = *str;
+					_bff_[_buff_ct++] = *str;
 			}
 		}
 		else
-			_bff_[_buff_count++] = (*fmt_ptr); /* Normal character */
-		/* Move to the next character in the format string */
+			_bff_[_buff_ct++] = (*fmt_ptr); /* Normal character */
+		
+		if (_buff_ct >= _bff_sz - 1)
+		{
+			_bff_[_buff_ct] = '\0';
+			_print_str_(_bff_);
+			_buff_ct = 0;
+
+		}
+
 	}
-	_bff_[_buff_count] = '\0'; /* Null-terminate the buffer */
+	_bff_[_buff_ct] = '\0'; /* Null-terminate the buffer */
 	_print_str_(_bff_); /* Print the formatted string */
 	va_end(_list_); /* End processing of variable arguments */
-	return (_buff_count); /* Return the total character count */
+	return (_buff_ct); /* Return the total character count */
 }
