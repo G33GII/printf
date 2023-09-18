d613bb08ed378f326db37e231d6c7b23f26c8702
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-18 10:19:03 +0100

    modified buffer statements

--- _printfII.c
+++ _printfII.c
@@ -4,9 +4,10 @@
  * @format: The format string.
  * Return: The total number of characters printed.
  */
+int _printf(const char *const format, ...);
 int _printf(const char *const format, ...)
 {
-	int _buff_ct = 0;             /* Character count in the buffer */
+	int _buff_ct = 0, _length_;             /* Character count in the buffer */
 	char _bff_[_bff_sz];            /* Buffer to store formatted string */
 	va_list _list_;                 /* Initialize a variable argument list */
 	const char *fmt_ptr = format;   /* Pointer to the format string */
@@ -23,10 +24,10 @@ int _printf(const char *const format, ...)
 			fmt_ptr++; /* Move to the character after '%' */
 			if (*fmt_ptr == '%')
 				_bff_[_buff_ct++] = '%';
+			else if (_buff_ct == _bff_sz)
+				_print_str_(_bff_);
 			else if (*fmt_ptr == 'c') /* Character format specifier */
-			{
 				_bff_[_buff_ct++] = va_arg(_list_, int);
-			}
 			else if (*fmt_ptr == 's') /* String format specifier */
 			{
 				char *str = va_arg(_list_, char *);
@@ -42,10 +43,9 @@ int _printf(const char *const format, ...)
 		{
 			_bff_[_buff_ct] = '\0';
 			_print_str_(_bff_);
+			_length_ += _buff_ct;
 			_buff_ct = 0;
-
 		}
-
 	}
 	_bff_[_buff_ct] = '\0'; /* Null-terminate the buffer */
 	_print_str_(_bff_); /* Print the formatted string */
